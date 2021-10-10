a625148d526f4b0f5452f633670ebbda2238830c
Nuzu <81609488+muezza04@users.noreply.github.com>
2021-10-10 19:18:44 +0700

    DDP

---
+++ opArt2.py
@@ -0,0 +1,15 @@
+sisi = 5
+panjang = 10
+lebar = 7
+luasPersegi = sisi * sisi
+kelPersegi = sisi * 4
+luasP_panjang = panjang * lebar
+kelP_panjang = (panjang + lebar) * 2
+matkul = "Dasar - dasar pemograman"
+lembaga = " di Nurul Fikri"
+title = matkul + lembaga
+print(title)
+print("\nLuas Persegi dengan sisi",sisi,"cm =",luasPersegi)
+print("\nKeliling Persegi =",kelPersegi)
+print("\nLuas Persegi Panjang =",luasP_panjang)
+print("\nKeliling Persegi Panjang =",kelP_panjang)
