26bc6b38c35cad5d8c6dcc0a2fde9c7f586080f6
Nuzu <81609488+muezza04@users.noreply.github.com>
2021-10-10 19:18:16 +0700

    DDP

---
+++ latVar2.py
@@ -0,0 +1,8 @@
+#latihan variabel
+nama = "Nuzurwan Patri Arja"
+gender = "L"
+umur = 18
+pekerjaan = "Mahasiswa"
+print("\nTipe data dari var nama : ",type(nama))
+print("Tipe data dari var gender : ",type(gender))
+print("Tipe data dari var umur : ",type(umur))
