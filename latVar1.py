1d9f7c46aa1eda6aa55a0a2f84df1ef0a9149bab
Nuzu <81609488+muezza04@users.noreply.github.com>
2021-10-10 19:17:44 +0700

    DDP

---
+++ latVar1.py
@@ -0,0 +1,7 @@
+#Latihan Variabel
+nama = "Nuzurwan Patri Arja"
+gender = "L"
+umur = 18
+pekerjaan = "Mahasiswa"
+print("Nama Mahasiswa : ",nama,"\nJenis Kelamin : ",gender,"\nUmur : ",umur," tahun","\nPekerjaan :",pekerjaan)
+print("tes")
