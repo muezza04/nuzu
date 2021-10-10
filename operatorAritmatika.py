62b9f0a66f7919927e18b057cb33b1e4caf0d2a1
Nuzu <81609488+muezza04@users.noreply.github.com>
2021-10-10 19:19:17 +0700

    DDP

---
+++ operatorAritmatika.py
@@ -0,0 +1,11 @@
+#belajar operator aritmatika
+jari2 = 15
+phi = 3.14
+luas = phi * jari2 * jari2
+keliling = 2 * phi * jari2
+matkul = "Belajar Pemrograman Python"
+lembaga = " di Nurul Fikri"
+title = matkul + lembaga
+print(title)
+print("\nLuas lingkaran dengan jari-jari ",jari2," cm = ", luas)
+print("\n dan kelilingnya =  ",keliling)
